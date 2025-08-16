8bc1965bc413fdd99a4d68bd5ce24fc13d54fdb7
PlayaOW <rayhanulkarim313@gmail.com>
2025-08-15 20:50:48 -0400

    Working on Resource contention detection and report sending

--- syslog.sh
+++ syslog.sh
@@ -13,6 +13,15 @@ if ! [[ -z "$LOGIN_LOGS" && "$COUNT" -le 10 ]]; then
     echo "$LOGIN_LOGS" | mail -s "Unauthorized Login Attempts and possible brute force attack detected" "$EMAIL"
     echo "Alert sent to $EMAIL"
     echo "Total unauthorized login attempts: $COUNT"
+    echo "Shutting down the system to prevent further unauthorized access."
+    shutdown -h now
 else
     echo "No unauthorized login attempts found."
 fi
+
+# Determining Resource contention and sending out report to SysAdmin
+TOP_PROCESSES_CPU=$(ps -eo user,pid,uid,comm,pcpu,pmem --sort=-pcpu | head -n 11) # Selects all processes and formats the output, sorts by CPU usage.
+TOP_PROCESSES_MEM=$(ps -eo user,pid,uid,comm,pcpu,pmem --sort=-pmem | head -n 11) # Selects all processes and formats the output, sorts by Memory Usage.
+
+SWAP_USAGE=$(top -b -n 1 | grep "MiB Swap" | awk '{print $4}')
+
